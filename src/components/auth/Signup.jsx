a0db97207a6e3cafb856a504e2378b321d4dcb9e
ReshmaR29 <reshmareddy2211@gmail.com>
2025-05-05 02:49:37 +0530

    resolved signup issue

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import Cookies from 'js-cookie';
 import { useAuth } from '../../context/AuthContext';
